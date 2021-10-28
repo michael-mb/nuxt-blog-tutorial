19b3b42975b593e25621a46e33bbdb0c56496899
michael-mb <mbonimichael@yahoo.fr>
2021-10-28 09:56:11 +0200

    init blog repository

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+body {
+  background-color: #EEE;
+}
+a {
+  text-decoration: none;
+}
+p {
+  margin-bottom: 15px;
+}
+p:last-of-type {
+  margin-bottom: 0px;
+}
+</style>
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,40 @@
+<template>
+  <article>
+    <!--{{article}}-->
+    <div>
+      <img :src="require(`~/assets/resources/${article.img}`)" alt="">
+    </div>
+    <nuxt-content :document="article"/>
+  </article>
+</template>
+
+<script>
+export default {
+
+  async asyncData({$content, params}){
+    const article = await $content('blog', params.slug).fetch();
+    return {article}
+  }
+}
+</script>
+
+<style scoped>
+img {
+  display: block;
+  width: 100%;
+  max-width: 500px;
+  margin: auto;
+}
+
+article {
+  display: block;
+  margin: 0 auto;
+  padding: 50px 30px;
+  max-width: 800px;
+}
+h1 {
+  font-size: 28px;
+  font-weight: 900;
+  margin-bottom: 30px;
+}
+</style>
